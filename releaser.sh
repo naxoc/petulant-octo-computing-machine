4675d6ecf4500a8223333cce82458af880f0f94c
Camilla Krag Jensen <ckj@information.dk>
2015-04-23 17:14:01 +0200

    Horsies.

--- releaser.sh
+++ releaser.sh
@@ -6,17 +6,19 @@ git pull origin develop
 git co master
 git pull origin master
 
+
+git merge develop
+
 TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
 NUMBER=`echo $TAG | grep -o "v[0-9]*" | grep -oE "[0-9]+"`
 NEXT=$(($NUMBER+1))
 RELEASEDATE=$(date +"%d/%m %Y %H:%M")
 NUMBER=4
 NOTES=`git log v$NUMBER..HEAD --pretty=format:'* %s' --no-merges`
-git merge develop --no-ff -e -m "Release v$NEXT
+git tag v$NEXT -m "Release v$NEXT
 $RELEASEDATE
 
 $NOTES"
 
-git tag v$NEXT
 git push origin master
 git push --tags
